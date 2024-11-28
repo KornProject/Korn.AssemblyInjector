3b63bbed4082c79871a8d431235a8b6be0f38909
Yotic <yoticcitoy@gmail.com>
2024-11-28 19:15:13 +0300

    Add project files.

---
+++ Korn.AssemblyInjector/Injector.cs
@@ -0,0 +1,36 @@
+﻿using System.Diagnostics;
+
+namespace Korn.AssemblyInjector;
+public class Injector : IDisposable
+{
+    public Injector(Process process)
+    {
+        Process = process;
+    }
+
+    public readonly Process Process;
+
+    nint unmanagedProcessHandle;
+    public void Inject(string assemblyPath)
+    {
+        const int PROCESS_ALL_ACCESS = 0x000F0000 | 0x00100000 | 0xFFFF;
+        var processHandle = Interop.OpenProcess(PROCESS_ALL_ACCESS, false, Process.Id);
+        unmanagedProcessHandle = processHandle;
+
+        var memoryBlob = MemoryBlob.Allocate(processHandle);
+
+
+    }
+
+    bool disposed;
+    public void Dispose()
+    {
+        if (disposed)
+            return;
+
+        if (unmanagedProcessHandle != 0)
+            Interop.CloseHandle(unmanagedProcessHandle);    
+    }
+
+    ~Injector() => Dispose();
+}
---
+++ Korn.AssemblyInjector/Internal/Interop.cs
@@ -0,0 +1,15 @@
+﻿using System.Runtime.InteropServices;
+
+static class Interop
+{
+    const string kernel = "kernel32";
+
+    [DllImport(kernel)] public static extern
+        nint OpenProcess(int desiredAccess, bool inheritHandle, int process);
+
+    [DllImport(kernel)] public static extern
+        bool CloseHandle(nint handle);
+
+    [DllImport(kernel)] public static extern 
+        nint VirtualAllocEx(nint process, nint address, uint size, uint allocationType, uint protect);
+}
