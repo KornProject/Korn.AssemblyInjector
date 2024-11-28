2d4ccce9424c248fec312034191d822c663e3adb
Yotic <yoticcitoy@gmail.com>
2024-11-28 19:17:24 +0300

    Fixed .gitignore file to allows \x86\ folders

---
+++ Korn.AssemblyInjector/x86/MemoryBlob.cs
@@ -0,0 +1,14 @@
+﻿record struct MemoryBlob(nint PageBase)
+{
+    public readonly nint PageBase;
+
+    public static MemoryBlob Allocate(nint processHandle, int initPageSize = 4096)
+    {
+        const int MEM_COMMIT = 0x1000;
+        const int PAGE_EXECUTE_READWRITE = 0x40;
+
+        var pagebase = Interop.VirtualAllocEx(processHandle, unchecked((nint)0x7FFFFFFFFFFFFFFFUL), (uint)initPageSize, MEM_COMMIT, PAGE_EXECUTE_READWRITE);
+
+        return new MemoryBlob(pagebase);
+    }
+}
